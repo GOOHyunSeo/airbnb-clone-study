f56014005960dccf22ef81d3d3c59e23fba7f39b
jeongjiyong <work.jjy1262@gmail.com>
2024-02-07 21:48:11 +0900

    detail 클릭 이벤트 추가

--- jandi/javascript/detail_1.js
+++ jandi/javascript/detail_1.js
@@ -1,22 +1,3 @@
-/*document.addEventListener("DOMContentLoaded", function () {
-    window.addEventListener("load", function () {
-        var boxesScrollmoving = document.querySelector(".boxes_scrollmoving");
-        var initialOffset = boxesScrollmoving.offsetTop;
-
-        window.addEventListener("scroll", function () {
-            var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-            if (scrollTop >= initialOffset) {
-                boxesScrollmoving.style.position = "fixed";
-                boxesScrollmoving.style.top = "0";
-            } else {
-                boxesScrollmoving.style.position = "static";
-                boxesScrollmoving.style.top = "auto";
-            }
-        });
-    });
-});*/
-
 document.addEventListener("DOMContentLoaded", function () {
     var targetElement = document.querySelector(".boxes_scrollmoving");
     var scrollStart = 700; // 특정 스크롤 범위의 시작 값
@@ -34,4 +15,62 @@ document.addEventListener("DOMContentLoaded", function () {
             // 특정 스크롤 범위 외에서는 다른 동작을 수행하거나 아무 동작도 하지 않음
         }
     });
+});
+
+function showReservationDate() {
+    var box2Inboxes = document.querySelector(".boxes_scrollmoving");
+    box2Inboxes.classList.add("active");
+}
+
+document.addEventListener("click", function (event) {
+    var showReservationDate = document.querySelector(".reservation_date");
+
+    if (event.target.closest(".box2_1") || event.target.closest(".box2_2")) {
+        showReservationDate.style.display = "block";
+    } else if (!event.target.closest(".reservation_date") && !event.target.closest(".box2_1")) {
+        showReservationDate.style.display = "none";
+    }
+});
+
+function closeReservationDate() {
+    var box2Inboxes = document.querySelector(".boxes_scrollmoving");
+    box2Inboxes.classList.add("active");
+}
+
+document.addEventListener("click", function (event) {
+    var closeReservationDate = document.querySelector(".reservation_date");
+
+    if (event.target.closest(".reservation_close")) {
+        closeReservationDate.style.display = "none";
+    } else {
+    }
+});
+
+function showGuestCalcualator() {
+    var box2Inboxes = document.querySelector(".boxes_scrollmoving");
+    box2Inboxes.classList.add("active");
+}
+
+document.addEventListener("click", function (event) {
+    var showGuestCalcualator = document.querySelector(".guest_calculator");
+
+    if (event.target.closest(".box2_3")) {
+        showGuestCalcualator.style.display = "block";
+    } else if (!event.target.closest(".guest_calculator")) {
+        showGuestCalcualator.style.display = "none";
+    }
+});
+
+function closeGuestCalcualator() {
+    var box2Inboxes = document.querySelector(".boxes_scrollmoving");
+    box2Inboxes.classList.add("active");
+}
+
+document.addEventListener("click", function (event) {
+    var closeGuestCalcualator = document.querySelector(".guest_calculator");
+
+    if (event.target.closest(".guest_close_button")) {
+        closeGuestCalcualator.style.display = "none";
+    } else {
+    }
 });
